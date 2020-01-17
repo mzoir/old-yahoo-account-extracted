852e02a8f727dcd723e6fe4cf9abec5e4dcf299d
Mzoir_zakariya <47563024+mzoir@users.noreply.github.com>
2020-01-17 15:42:47 +0100

    Email-generator.py

---
+++ ema.py
@@ -0,0 +1,14 @@
+import random
+
+
+
+
+
+
+num = ["a","b","c","d","j","k","l","m","n","k","l","q","i","g","h"]
+numb = [0,1,2,4,3,5]
+x=1
+y=0         			
+while True:    	                                                
+    x+=1
+    print "{}{}_{}{}@yahoo.com".format(random.choice(num),random.choice(num),random.choice(num),random.choice(num))
